532669abdadd81bd7ed1682412e5dba1f0763a7b
Evgenz-mr <45653581+Evgenz-mr@users.noreply.github.com>
2026-09-12 21:28:50 +0300

    Fix optional component installer

--- scripts/install-extras.ps1
+++ scripts/install-extras.ps1
@@ -9,7 +9,6 @@ $ErrorActionPreference='Stop';$context="kind-$Cluster"
 helm repo add bitnami https://charts.bitnami.com/bitnami --force-update
 helm repo add kyverno https://kyverno.github.io/kyverno/ --force-update
 helm repo update
-if($Redis-eq'enabled'){}
 if($Redis-eq'enabled){helm upgrade --install redis bitnami/redis --kube-context $context -n data --create-namespace --set architecture=standalone --set auth.enabled=false --set master.persistence.enabled=false;if($LASTEXITCODE-ne 0){throw 'Redis install failed.'}}
 if($Kafka-eq'enabled){helm upgrade --install kafka bitnami/kafka --kube-context $context -n data --create-namespace --set controller.replicaCount=1 --set broker.replicaCount=0 --set persistence.enabled=false;if($LASTEXITCODE-ne 0){throw 'Kafka install failed.'}}
 if($MinIO-eq'enabled){helm upgrade --install minio bitnami/minio --kube-context $context -n data --create-namespace --set auth.rootUser=admin --set auth.rootPassword=adminadmin --set defaultBuckets=app --set persistence.enabled=false;if($LASTEXITCODE-ne 0){throw 'MinIO install failed.'}}
